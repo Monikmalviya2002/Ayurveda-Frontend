dd1e34fdbcdb4fc40963eb10fac4b6b4dcee21c2
tauhidst07 <atauhid07@gmail.com>
2025-11-17 18:08:07 +0530

    local changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ import Home from './pages/HomePage';
 import { BlogsPage } from './pages/BlogsPage';
 import BlogDetailsPage from './pages/BlogDetailsPage';
 import VideoDashboard from './components/VideoDashboard';
-import DoctorDirectoryPage from './pages/DoctorsDirectoryPage';
+import DoctorDirectoryPage from './pages/DoctorsDirectoryPage'; 
+import AdminPage from './pages/Admin/AdminPage'
+import ConsultationBookingPage from './pages/ConsultationBookingPage';
+
+
 
 export default function App() {
   return (
@@ -17,7 +21,9 @@ export default function App() {
           <Route path='/blogs' element={<BlogsPage/>} /> 
           <Route path='/blogs/:blogId' element={<BlogDetailsPage/>}/>
           <Route path='/videos' element={<VideoDashboard/>}/> 
-          <Route path='/doctors' element={<DoctorDirectoryPage/>} />
+          <Route path='/doctors' element={<DoctorDirectoryPage/>} /> 
+          <Route path='/admin' element={<AdminPage/>} />   
+          <Route path='/consult' element={<ConsultationBookingPage/>}/>
           <Route path="*" element={<div className="p-8 text-center">Page not found</div>} />
         </Routes>
       </main>
