6ae9818c7d912dc6038845454db1483dfe21226f
tauhidst07 <atauhid07@gmail.com>
2025-11-19 16:59:58 +0530

    fix App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
 import Home from './pages/HomePage';
-import Login from './pages/Authentication/Login';
-import Signup from './pages/Authentication/Signup';
 import { BlogsPage } from './pages/BlogsPage';
 import BlogDetailsPage from './pages/BlogDetailsPage';
 import VideoDashboard from './components/VideoDashboard';
@@ -11,15 +8,17 @@ import DoctorDirectoryPage from './pages/DoctorsDirectoryPage';
 import DoctorProfile from './pages/DoctorProfile'; 
 import AdminPage from './pages/Admin/AdminPage'
 import BlogCreateForm from './pages/Doctor/BlogCreateForm.jsx'
-
+import ConsultationBookingPage from './pages/ConsultationBookingPage.jsx'; 
+import Login from "./pages/Authentication/Login.jsx"
+import Signup from "./pages/Authentication/Signup.jsx"
 export default function App() {
   return (
     <Router>
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />  
+          <Route path='/signup' element={<Signup/>}/>
           <Route path='/blogs' element={<BlogsPage/>} /> 
           <Route path='/blogs/:blogId' element={<BlogDetailsPage/>}/>
           <Route path='/videos' element={<VideoDashboard/>}/> 
@@ -27,6 +26,7 @@ export default function App() {
           <Route path ='/doctors/:id' element={<DoctorProfile/>}/> 
            <Route path='/doctor/writeBlog' element={<BlogCreateForm/>}/>
           <Route path='/admin' element={<AdminPage/>} /> 
+          <Route path='/testing' element={<ConsultationBookingPage/>}/>
           <Route path="*" element={<div className="p-8 text-center">Page not found</div>} />
         </Routes>
       </main>
